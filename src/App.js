54af7e2f4b74c6b06f38fb2483ef78b290cd29bd
WiSoft <125270437+Alphonce08@users.noreply.github.com>
2026-05-13 23:45:26 +0300

    ...Update...

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
-//import Projects from './components/Projects';
+import Projects from './components/Projects';
 import Education from './components/Education';
 import Contact from './components/Contact';
 
