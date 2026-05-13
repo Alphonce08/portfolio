4d59fc984abc30e398f019bbe3cc602c51d31f51
WiSoft <125270437+Alphonce08@users.noreply.github.com>
2026-05-14 00:10:24 +0300

    ...Update...
    Comment out the Projects component in the App function.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
-import Projects from './components/Projects';
+//import Projects from './components/Projects';
 import Education from './components/Education';
 import Contact from './components/Contact';
 
@@ -15,7 +15,7 @@ function App() {
         <Hero />
         <Experience />
         <Skills />
-        <Projects />
+       // <Projects />
         <Education />
         <Contact />
       </main>
